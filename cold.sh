dca6634b5235965fce19319d2d960572e1dc06b2
mux0x <87925079+mux0x@users.noreply.github.com>
2022-12-31 16:56:29 +0200

    Create cold.sh

---
+++ cold.sh
@@ -0,0 +1,7 @@
+line=$1
+echo $line | waybackurls | anew $line/urls.txt;
+echo $line | gau --threads 5 | anew $line/urls.txt
+echo $line | katana -d 5 silent | anew $line/urls.txt
+echo $line | hakrawler | anew $line/urls.txt
+cat $line/urls.txt| sed '$!N; /^\(.*\)\n\1$/!P; D'| grep -P '\.php|\.asp|\.js|\.jsp' > $line/endpoints.txt
+cat $line/urls.txt| grep -Po '(?:\?|\&)(?<key>[\w]+)(?:\=|\&?)(?<value>[\w+,.-]*)' | tr -d '?' | tr -d '&' > $line/parameters.txt
