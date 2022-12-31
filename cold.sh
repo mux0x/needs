965ad88a94c06bc4ecd24d12a61f51ddcd9527bd
mux0x <87925079+mux0x@users.noreply.github.com>
2022-12-31 17:09:51 +0200

    Update cold.sh

--- cold.sh
+++ cold.sh
@@ -3,5 +3,5 @@ echo $line | waybackurls | anew $line/urls.txt;
 echo $line | gau --threads 5 | anew $line/urls.txt
 echo $line | katana -d 5 silent | anew $line/urls.txt
 echo $line | hakrawler | anew $line/urls.txt
-cat $line/urls.txt| sed '$!N; /^\(.*\)\n\1$/!P; D'| grep -P '\.php|\.asp|\.js|\.jsp' > $line/endpoints.txt
-cat $line/urls.txt| grep -Po '(?:\?|\&)(?<key>[\w]+)(?:\=|\&?)(?<value>[\w+,.-]*)' | tr -d '?' | tr -d '&' > $line/parameters.txt
+cat $line/urls.txt| sed '$!N; /^\(.*\)\n\1$/!P; D'| grep -P '\.php|\.asp|\.js|\.jsp' | anew $line/endpoints.txt
+cat $line/urls.txt| grep -Po '(?:\?|\&)(?<key>[\w]+)(?:\=|\&?)(?<value>[\w+,.-]*)' | tr -d '?' | tr -d '&' | anew $line/parameters.txt
