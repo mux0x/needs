a974872a1b7a07ea8ac804da858f20c389b849e6
Muhammed K. Sayed <87925079+mux0x@users.noreply.github.com>
2023-01-02 22:16:24 +0200

    Update cold.sh

--- cold.sh
+++ cold.sh
@@ -4,4 +4,4 @@ echo $line | gau --threads 5 | anew $line/urls.txt
 echo $line | katana -d 5 silent | anew $line/urls.txt
 echo $line | hakrawler | anew $line/urls.txt
 cat $line/urls.txt| sed '$!N; /^\(.*\)\n\1$/!P; D'| grep -P '\.php|\.asp|\.js|\.jsp' | anew $line/endpoints.txt
-cat $line/urls.txt| grep -Po '(?:\?|\&)(?<key>[\w]+)(?:\=|\&?)(?<value>[\w+,.-]*)' | tr -d '?' | tr -d '&' | anew $line/parameters.txt
+cat $line/urls.txt| grep -Po '(?:\?|\&)(?<key>[\w]+)(?:\=|\&?)(?<value>[\w+,.-]*)' | tr -d '?' | tr -d '&' | sort -u | uniq | anew $line/parameters.txt
