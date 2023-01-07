aa7ccb28524a76af69c8110813c8237d4856468a
Muhammed K. Sayed <87925079+mux0x@users.noreply.github.com>
2023-01-07 23:06:34 +0200

    Update cold.sh

--- cold.sh
+++ cold.sh
@@ -1,8 +1,11 @@
 line=$1
 outputDir=$2
 echo $line | waybackurls | anew $outputDir/urls.txt;
-echo $line | gau --threads 5 | anew $outputDir/urls.txt
-echo $line | katana -d 5 silent | anew $outputDir/urls.txt
-echo $line | hakrawler | anew $outputDir/urls.txt
+echo https://$line | gau --threads 5 | anew $outputDir/urls.txt
+echo http://$line | gau --threads 5 | anew $outputDir/urls.txt
+echo https://$line | katana -d 5 silent | anew $outputDir/urls.txt
+echo http://$line | katana -d 5 silent | anew $outputDir/urls.txt
+echo https://$line | hakrawler | anew $outputDir/urls.txt
+echo http://$line | hakrawler | anew $outputDir/urls.txt
 cat $outputDir/urls.txt| sed '$!N; /^\(.*\)\n\1$/!P; D'| grep -P '\.php|\.asp|\.js|\.jsp' | anew $outputDir/endpoints.txt
 cat $outputDir/urls.txt| grep -Po '(?:\?|\&)(?<key>[\w]+)(?:\=|\&?)(?<value>[\w+,.-]*)' | tr -d '?' | tr -d '&' | sort -u | uniq | anew $outputDir/parameters.txt
